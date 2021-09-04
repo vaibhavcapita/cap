80a16b24ee09992f37096d6ed944c4c7f9d705b4
vaibhav <gatne.vaibhav@gmail.com>
2021-09-04 15:11:17 +0530

    add code

--- src/java/test.java
+++ src/java/test.java
@@ -10,3 +10,29 @@ else {
 	system.out.print("it works fine on IE");
 }
 
+
+if(browser=="chrome")
+{
+	system.out.print("it works fine");
+}
+else {
+	system.out.print("it works fine on IE");
+}
+
+if(browser=="chrome")
+{
+	system.out.print("it works fine");
+}
+else {
+	system.out.print("it works fine on IE");
+}
+
+if(browser=="chrome")
+{
+	system.out.print("it works fine");
+}
+else {
+	system.out.print("it works fine on IE");
+}
+
+
